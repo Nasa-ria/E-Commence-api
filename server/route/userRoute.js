6b388ce4f53cdc72058bf014c9dfaaedd372dd78
ria-hue <mumuninasaria@gmail.com>
2023-10-31 21:54:13 -0700

    merge code

--- server/route/userRoute.js
+++ server/route/userRoute.js
@@ -5,92 +5,12 @@ const controller = require("../controller/userController");
 const router = require("express").Router();
 
 
-<<<<<<< HEAD
-passport.use(
-	new LocalStrategy({ passReqToCallback: true }, async function verify(req,username,password,cb) {
-		// checks if the phone number inputed  matches
-		const user = await User.findOne({ email:username });
-        // console.log(user)
-		if (user) {
-			//   user attempt
-			// if (user.status) {
-				const passwordVerified = await bcrypt.compare(password ,user.password)
-				//   and the password too
-				if(passwordVerified){
-					return cb(null, user); /* verification succesfull */
-				
-			}
-			// } else {
-			// 	return cb(null, false, {
-			// 		message: "your account has been deactivated",
-			// 	});
-			// }
-		}
-		// Tracklogin(req, user);
-		return cb(null, false, {
-			message: "incorrect  email or password",
-		}); /* verification failed */
-	}),
-);
-
-
-// const Tracklogin = async (req, user) => {
-// 	const session = req.session;
-// 	if (!session.maxFailedAttempts) {
-// 		session.maxFailedAttempts = 5;
-// 	} else {
-// 		session.maxFailedAttempts -= 1;
-// 		const maxFailedAttempts = session.maxFailedAttempts;
-// 		if (maxFailedAttempts <= 1) {
-// 			user.status = false;
-// 			await user.save();
-// 		}
-// 	}
-
-// };
-passport.serializeUser(function (user, cb) {
-	return cb(null, user);
-});
-
-  passport.deserializeUser(function (user, cb) {
-	return cb(null, user);
-});
-
-router.use(passport.initialize());
-router.use(passport.session());
-
-
-router.get("/user/login", controller.login);
-// outsourcing the authentication to  passport
-router.post('/user/login', passport.authenticate("local", {
-    failureRedirect: "/user/login",
-    failureFlash: true,
-}), controller.authenticatelogin);
-
-
-// router.post("/user/register", controller.register);
-
-const isAuthenticated = (req, res, next) => {
-	if (req.isAuthenticated()) {
-		return next(); 
-	  }
-	  res.redirect("/user/login"); 
-  };
-
-//   router.use(isAuthenticated)
-// router.use(loginAuthentication)
-router.post("/user/register", controller.register);
-router.post("/user/changePassword",controller.changePassword);
-router.get("/user/edit/:id", controller.edit);
-router.post("/user/update/:id",controller.update);
-=======
 
 
 router.post("/user/login", controller.authenticatelogin);
 router.post("/user/register" ,controller.register);
 router.post("/user/changePassword",authenticateToken,controller.changePassword);
 router.post("/user/update/:id",authenticateToken,controller.update);
->>>>>>> 2d3bee4cb80f96e8a819aa952f3784a3cc88c8d4
 router.get("/user/user/:id", controller.user);
 router.get("/user/users", controller.users);
 router.delete("/user/delete/:id",authenticateToken, controller.delete);
